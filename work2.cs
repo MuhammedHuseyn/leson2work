5dd916fe23a2ce12dc2e1d7f1a8d77085acce3d1
Muhammed Huseyn <salamovsalam1w@gmail.com>
2023-07-19 22:16:15 +0400

    change

---
+++ work2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection.Metadata;
+
+namespace LessonOne
+{
+    class HelloWorld
+    {
+        private static int num;
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter the temperature in Celsius:");
+            //int numr34;
+            //numr34 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("The temperature in Fahrenheit is:{0}", (numr34 * 9) / 5 + 32);
+            }
+            }
